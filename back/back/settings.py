5fccf8555a6b80b0aeb76a7baf6e142d64c39c99
Zhenyettta <zhecky2004@gmail.com>
2023-05-06 20:31:53 +0300

    settings refactor

--- back/back/settings.py
+++ back/back/settings.py
@@ -73,10 +73,6 @@ WSGI_APPLICATION = 'back.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': '3LAGODA',
